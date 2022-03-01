c53f78023db8385037838376419d32ea694a927b
MariyaStrudel <72531399+MariyaStrudel@users.noreply.github.com>
2022-03-01 11:30:00 -0500

    Update PointsParser.java

--- directionhelpers/PointsParser.java
+++ directionhelpers/PointsParser.java
@@ -1,12 +1,10 @@
 package com.example.mapproj.directionhelpers;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.os.AsyncTask;
 import android.util.Log;
 
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.PolylineOptions;
 
 import org.json.JSONObject;
 
@@ -18,9 +16,12 @@ import java.util.List;
  * Created by Vishal on 10/20/2018.
  */
 
+
+
 public class PointsParser extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
     TaskLoadedCallback taskCallback;
     String directionMode = "driving";
+    ArrayList<LatLng> points;
 
     public PointsParser(Context mContext, String directionMode) {
         this.taskCallback = (TaskLoadedCallback) mContext;
@@ -50,17 +51,17 @@ public class PointsParser extends AsyncTask<String, Integer, List<List<HashMap<S
             e.printStackTrace();
         }
         return routes;
-    }
 
-    // Executes in UI thread, after the parsing process
+        }
+//         Executes in UI thread, after the parsing process
     @Override
     protected void onPostExecute(List<List<HashMap<String, String>>> result) {
-        ArrayList<LatLng> points;
-        PolylineOptions lineOptions = null;
+//        PolylineOptions lineOptions = null;
         // Traversing through all the routes
         for (int i = 0; i < result.size(); i++) {
             points = new ArrayList<>();
-            lineOptions = new PolylineOptions();
+//            lineOptions = new PolylineOptions();
+            //Random Coord Generation
             // Fetching i-th route
             List<HashMap<String, String>> path = result.get(i);
             // Fetching all the points in i-th route
@@ -71,25 +72,28 @@ public class PointsParser extends AsyncTask<String, Integer, List<List<HashMap<S
                 LatLng position = new LatLng(lat, lng);
                 points.add(position);
             }
-            // Adding all the points in the route to LineOptions
-            lineOptions.addAll(points);
-            if (directionMode.equalsIgnoreCase("walking")) {
-                lineOptions.width(10);
-                lineOptions.color(Color.MAGENTA);
-            } else {
-                lineOptions.width(20);
-                lineOptions.color(Color.BLUE);
-            }
-            Log.d("mylog", "onPostExecute lineoptions decoded");
-        }
 
+//            // Adding all the points in the route to LineOptions
+//            lineOptions.addAll(points);
+//            if (directionMode.equalsIgnoreCase("walking")) {
+//                lineOptions.width(10);
+//                lineOptions.color(Color.MAGENTA);
+//            } else {
+//                lineOptions.width(20);
+//                lineOptions.color(Color.BLUE);
+//            }
+//            Log.d("mylog", "onPostExecute lineoptions decoded");
+        }
+        taskCallback.onTaskDone(points);
         // Drawing polyline in the Google Map for the i-th route
-        if (lineOptions != null) {
-            //mMap.addPolyline(lineOptions);
-            taskCallback.onTaskDone(lineOptions);
-
-        } else {
-            Log.d("mylog", "without Polylines drawn");
-        }
+//        if (lineOptions != null) {
+//            //mMap.addPolyline(lineOptions);
+//
+//            taskCallback.onTaskDone(lineOptions);
+//
+//        } else {
+//            Log.d("mylog", "without Polylines drawn");
+////        }
+//        }
     }
 }
